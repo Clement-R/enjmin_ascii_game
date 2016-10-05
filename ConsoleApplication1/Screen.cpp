e1665c772315910130df38f2efbaa79fb9116754
Clement-R <rondeau.clement@gmail.com>
2016-10-05 15:43:17 +0200

    Fix wrong call to static attributes

--- ConsoleApplication1/Screen.cpp
+++ ConsoleApplication1/Screen.cpp
@@ -4,9 +4,9 @@
 
 Screen::Screen()
 {
-	this->dwBufferSize = { this->SCREEN_WIDTH, this->SCREEN_HEIGHT };
+	this->dwBufferSize = { Screen::SCREEN_WIDTH, Screen::SCREEN_HEIGHT };
 	this->dwBufferCoord = { 0, 0 };
-	this->rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
+	this->rcRegion = { 0, 0, Screen::SCREEN_WIDTH - 1, Screen::SCREEN_HEIGHT - 1 };
 	this->hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
 
 	SetConsoleScreenBufferSize(this->hOutput, this->dwBufferSize);
